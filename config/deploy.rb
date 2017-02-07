d581c5ab9ae5609764d561ee1f2ec4efb6d628ea
sheff3rd <thedoctorwhoovesmail@gmail.com>
2017-02-07 10:52:43 +0200

    Set up Node, Npm, Angular 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,30 @@
-server '188.166.174.146', roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@github.com:Sheff3rd/active-prok.git'
-set :application,     'active-prok'
-set :user,            'deploy'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+branch = ENV['BRANCH'] || 'master'
 
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :application, 'active-prok'
+set :repo_url, 'git@github.com:Sheff3rd/active-prok.git'
+set :passenger_restart_with_touch, true
+set :deploy_to, '/home/deploy/apps/active-prok'
+set :linked_files, %w(config/database.yml config/secrets.yml)
+set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system node_modules)
+
+# puma
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_init_active_record, true
 set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :branch, branch
 
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
-
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :workers, default: 2, mailers: 1
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -44,18 +38,27 @@ namespace :puma do
   before :start, :make_dirs
 end
 
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
+namespace :npm do
+  task :install do
+    on roles :app do
+      within release_path do
+        execute :npm, :install
       end
     end
   end
+end
 
+namespace :npm do
+  task :start do
+    on roles :app do
+      within release_path do
+        execute :npm, :start
+      end
+    end
+  end
+end
+
+namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
@@ -64,19 +67,10 @@ namespace :deploy do
     end
   end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
 
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
+after 'deploy:updating', 'npm:install'
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+[![CircleCI](https://circleci.com/gh/Sheff3rd/active-prok.svg?style=svg)](https://circleci.com/gh/Sheff3rd/active-prok)
+-
+
+Setup
+-
+
+    git clone
+    bundle install
+    rake db:create db:migrate
+    npm install
+    npm start
+    rails s
+-
